0e1230474aac992fb8262952c21102de818b9ded
Daniel Puc May <skar.dpm@gmail.com>
2021-06-24 20:25:33 -0500

    land-funkos-v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './componets/banner/banner.component';
+import { ContactComponent } from './componets/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:BannerComponent},
+  {path:'quienes-somos',component:BannerComponent},
+  {path:'contactanos',component:ContactComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+/*Componentes*/
+import {HeaderComponent}from './componets/header/header.component';
+import {BannerComponent}from './componets/banner/banner.component';
+import {FooterComponent}from './componets/footer/footer.component';
+import {ContactComponent}from './componets/contact/contact.component';
+
+/*Animaciones */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/*Componentes de material */
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
