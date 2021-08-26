6fb4e2667fa92eb15f3ab7bd2bf8ff5b3e615263
Daniel Puc May <skar.dpm@gmail.com>
2021-08-26 02:34:35 -0500

    agosto 26 v2

--- src/app/componets/banner/banner.component.ts
+++ src/app/componets/banner/banner.component.ts
@@ -1,16 +1,19 @@
 import {Component} from '@angular/core';
 import { UserHttpServiceService } from 'src/app/user-http-service.service';
-
-
+import { OnInit } from '@angular/core';
 @Component({
     selector:'app-banner',
     templateUrl:'./banner.component.html',
     styleUrls: ['./banner.component.scss'],
 
 })
-export class BannerComponent{
+export class BannerComponent implements OnInit{
 
 constructor(private service:UserHttpServiceService){}
+users:string[]=[];
+ngOnInit() {
+}
+
 
 public async httpRequest(){
     const query : any = await this.service.login({user:'Daniel', password:'123'});
@@ -21,11 +24,26 @@ public async httpRequest(){
 
 }
 
+
+
+
+
+
 public async httpRequestToken(){
     const query : any = await this.service.getUsers({});
     console.log(query);
+    for (let i = 0; i < query.httpRequest.data.data.length; i++) {
+       
+        this.users.push(query.httpRequest.data.data[i].name);
+       
+    }
+    
+    console.log(this.users);
 
 }
+
+
+
 
 }
 
